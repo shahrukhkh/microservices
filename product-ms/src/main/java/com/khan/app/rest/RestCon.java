07e841ef091642e942ce1deec56c976228280d84
Shahrukh Khan <khanshahrukh7910@gmail.com>
2025-11-10 11:06:04 +0530

    commit

---
+++ product-ms/src/main/java/com/khan/app/rest/RestCon.java
@@ -0,0 +1,17 @@
+package com.khan.app.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class RestCon {
+	
+	@GetMapping("/msg")
+	public String getMsg()
+	{
+		return "Order";
+	}
+
+}
